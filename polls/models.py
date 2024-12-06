ea3c52c0ac663ecf03dd71af9568fea4d65053d1
dojunhee <dodo@dojunheeui-MacBookPro.local>
2024-12-06 15:34:02 +0900

    serialize test

--- polls/models.py
+++ polls/models.py
@@ -6,12 +6,12 @@ import json
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    def __str__(self):
-        return json.dumps(model_to_dict(self), cls=DjangoJSONEncoder, ensure_ascii=False)
+    # def __str__(self):
+    #     return json.dumps(model_to_dict(self), cls=DjangoJSONEncoder, ensure_ascii=False)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):
-        return json.dumps(model_to_dict(self), cls=DjangoJSONEncoder, ensure_ascii=False)
+    # def __str__(self):
+    #     return json.dumps(model_to_dict(self), cls=DjangoJSONEncoder, ensure_ascii=False)

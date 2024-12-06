61d2d149213e0f8812cb52ede919d1c56dc1dad4
dojunhee <dodo@dojunheeui-MacBookPro.local>
2024-12-06 14:04:47 +0900

    add load .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv() # .env 파일 로드
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
